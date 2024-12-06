a7eec52e8b5b139599ce11f3633ac9dadba5fbe1
Touki-dev <touki.dev@gmail.com>
2024-12-06 09:19:17 +0100

    wip

--- 2023/Day 25/day25.py
+++ 2023/Day 25/day25.py
@@ -4,20 +4,22 @@ def read_file(file):
         return [(i[0], list(i[1].split())) for i in (line.split(': ') for line in file)]
 
 l = read_file("input.txt")
-# Chargement du graphe (liste d'arêtes par exemple)
-G = nx.Graph()
-edges = [(i[0], j) for i in l for j in i[1]]
-G.add_edges_from(edges)
 
-# Trouver une coupe minimale
-cut_set = nx.minimum_edge_cut(G)
+# Chargement du graphe avec les listes de connexions
+GRAPHE = nx.Graph()
+edges = [(i[0], j) for i in l for j in i[1]]
+GRAPHE.add_edges_from(edges)
+
+# Trouver une coupe minimale, surement de 3 arêtes
+print("Coupe...")
+cut_set = nx.minimum_edge_cut(GRAPHE)
 
 # Afficher les arêtes dans la coupe
 print("Edges in the Min-Cut:", cut_set)
 
 # Supprimer les arêtes de la coupe
-G.remove_edges_from(cut_set)
+GRAPHE.remove_edges_from(cut_set)
 
 # Trouver les composantes connectées
-components = list(nx.connected_components(G))
+components = list(nx.connected_components(GRAPHE))
 print("produit of Components", len(components[0]) * len(components[1]))
