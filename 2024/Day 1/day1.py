733a84e1d94b9ef27a57fe6e8aa681781410632d
Gaspard <136388622+Touki-dev@users.noreply.github.com>
2024-12-06 13:51:25 +0100

    Update day1.py

--- 2024/Day 1/day1.py
+++ 2024/Day 1/day1.py
@@ -1,31 +1,18 @@
 INPUT = open("input.txt", "r")
-INPUT = INPUT.read().split('\n')
+INPUT = [(int(i.split('   ')[0]), int(i.split('   ')[1])) for i in INPUT.read().split('\n')]
 
 def part1(l1, l2):
-    for i in range(len(INPUT)):
-        nombres = INPUT[i].split('   ')
-        n1 = nombres[0]
-        n2 = nombres[1]
-        
-        l1.append(int(n1))
-        l2.append(int(n2))
-
     total = 0
     for i in range(len(l1)):
         total += abs(min(l1)-min(l2))
-        
+
         l1.pop(l1.index(min(l1)))
         l2.pop(l2.index(min(l2)))
+
     return total
 
 def part2(l1, l2):
-    for i in range(len(INPUT)):
-        nombres = INPUT[i].split('   ')
-        n1 = nombres[0]
-        n2 = nombres[1]
-        
-        l1.append(int(n1))
-        l2.append(int(n2))
+    print(len(l1))
 
     total = 0
     for i in range(len(l1)):
@@ -33,11 +20,13 @@ def part2(l1, l2):
         for j in range(len(l2)):
             if l1[i] == l2[j]:
                 augmentation += 1
-                
+
         total += l1[i] * augmentation
     return total
-    
+
 if __name__ == "__main__":
-    l1, l2 = [], []
-    print(f"Résulat de la partie 1 : {part1(l1, l2)}")
+    l1 = list(map(lambda el: el[0], INPUT))
+    l2 = list(map(lambda el: el[1], INPUT))
+    print(f"Résulat de la partie 1 : {part1(list(l1), list(l2))}")
     print(f"Résulat de la partie 2 : {part2(l1, l2)}")
+
