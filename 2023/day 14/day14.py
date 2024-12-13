add1a402a21b8d917fcca6cc944ade3bf29585a3
Touki-dev <touki.dev@gmail.com>
2024-12-13 22:02:07 +0100

    day14 2023

---
+++ 2023/day 14/day14.py
@@ -0,0 +1,53 @@
+import numpy as np
+from tqdm import tqdm
+
+def read_file(file):
+    with open(file, 'r') as file:
+        lignes = file.read().split('\n')
+        return np.array(list(map(list, lignes)))
+
+def simulate_rolling(rock, l, d):
+    x,y = rock
+    nx = x
+    for i in range(rock[0]):
+        nx, ny = x + d[0], y + d[1]
+        if 0 <= nx < len(l) and 0 <= ny < len(l[0]) and l[nx,ny] == ".":
+            x, y = nx, ny
+        else:
+            return (x,y)
+    return (x,y)
+
+DIRS = ((-1,0),(0,1),(1,0),(0,-1))
+
+def part1(l):
+    rounded_rocks = [coord for coord in np.ndindex(l.shape) if l[coord] == "O"]
+    for rock in rounded_rocks:
+        new_pos = simulate_rolling(rock, l, (-1, 0))
+        if new_pos:
+            l[rock], l[new_pos] = ".", "O"
+        
+    rounded_rocks = [coord for coord in np.ndindex(l.shape) if l[coord] == "O"]
+    total = 0
+    for rock in rounded_rocks:
+        total += len(l) - rock[0]
+    return total
+
+def part2(l):
+    rounded_rocks = [coord for coord in np.ndindex(l.shape) if l[coord] == "O"]
+    for _ in tqdm(range(1000000000)):
+        for d in DIRS:
+            for rock in rounded_rocks:
+                new_pos = simulate_rolling(rock, l, d)
+                if new_pos:
+                    l[rock], l[new_pos] = ".", "O"
+        
+    rounded_rocks = [coord for coord in np.ndindex(l.shape) if l[coord] == "O"]
+    total = 0
+    for rock in rounded_rocks:
+        total += len(l) - rock[0]
+    return total
+
+if __name__ == "__main__":
+    l = read_file("input.txt")
+    print(f"Résulat de la partie 1 : {part1(l)}")
+    print(f"Résulat de la partie 2 : {part2(l)}")
