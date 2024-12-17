f8f2199edf34849bc2b81c31898ba173c6b59fcb
Touki-dev <touki.dev@gmail.com>
2024-12-17 20:42:56 +0100

    Update template

--- template.py
+++ template.py
@@ -1,14 +1,6 @@
-FORMAT = 1
-
 def read_file(file):
     with open(file, 'r') as file:
         lignes = file.read().split('\n')
-        if FORMAT == 0: # Lignes
-            return lignes
-        if FORMAT == 1: # Grid
-            return list(map(list, lignes))
-        if FORMAT == 2: # Custom
-            return [(int(i.split(': ')[0]), list(map(int, i.split(': ')[1].split()))) for i in lignes]
 
 def part1(l):
     ...
